826872f8b1387912c35151dda39a2cc18494a300
schwaebek <schwaebek@Katlyns-MacBook-Pro.local>
2014-07-23 10:15:55 -0400

    create list app

---
+++ List App/List App/MFLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFLAppDelegate.h
+//  List App
+//
+//  Created by Katlyn Schwaebe on 7/23/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ List App/List App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  List App
+//
+//  Created by Katlyn Schwaebe on 7/23/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFLAppDelegate class]));
+    }
+}
